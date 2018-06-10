f6a1073583baa54fe31a2487631706497f1de99f
白衬衫 <codingforfun@lihuilongdeMacBook-Pro.local>
2018-06-11 00:01:43 +0800

    client demo add connect function

--- src/client/Assets/Script/start.cs
+++ src/client/Assets/Script/start.cs
@@ -1,16 +1,71 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class start : MonoBehaviour {
 
+	public Text ip_;
+	public Text port_;
+	public Text name_;
+	public Text psd_;
+	public Text msg_;
+
+	private netdemo net_ = new netdemo();
+
 	// Use this for initialization
 	void Start () {
-		
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		string str = net_.GetViewMsg ();
+		if (!string.IsNullOrEmpty (str)) {
+			Debug.Log (str);
+			msg_.text = str;
+		}
+	}
+
+	void OnDestroy()
+	{
+	}
+
+	public void OnBtnDisconnect()
+	{
+		net_.Shutdown ();
+
+	}
+
+	//demo for login
+	public void OnBtnLogin()
+	{
+		if (string.IsNullOrEmpty (ip_.text)) 
+		{
+			Debug.LogWarning ("please input ip...");
+			return;
+		}
+		if (string.IsNullOrEmpty (port_.text)) 
+		{
+			Debug.LogWarning("please input port...");
+			return;
+		}
+		if (string.IsNullOrEmpty (name_.text)) 
+		{
+			Debug.LogWarning ("please input name...");
+			return;
+		}
+		if (string.IsNullOrEmpty (psd_.text)) 
+		{
+			Debug.LogWarning ("please input password...");
+			return;
+		}
+
+		//connect server
+		net_.ConnectServer(ip_.text, System.Convert.ToInt32(port_.text));
+
+		//do shakehands
+
+		//login
+
 	}
 }
