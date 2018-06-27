50f0e1a9b868850049d75a210699aa8ccf43b695
白衬衫 <codingforfun@lihuilongdeMBP.lan>
2018-06-27 23:33:35 +0800

    解析基本TCP包，还剩下protobuf lua decode

--- src/server/gate/gates.lua
+++ src/server/gate/gates.lua
@@ -3,7 +3,8 @@ local gateserver = require "snax.gateserver"
 local servicemsg = require "servicemsg"
 
 local handler = {}
-local agent = {}
+local agent = {} --玩家代理
+local service = {} --所有注册的服务
 
 function handler.connect(fd, ipaddr)
     print("client:fd["..fd.."]ip["..ipaddr.."] has connected")
@@ -40,46 +41,26 @@ function handler.error(fd, msg)
     agent[fd] = nil
 end
 
+--消息分发
 function handler.message(fd, msg, sz)
     print("recv client msg:fd["..fd.."]msg["..msg.."]sz["..sz.."]")
     
     --消息解包：1byte（服务id）+ 2byte（消息id，Big-Endian 编码）+ 消息体（protobuf序列化的包）
     if sz >= 3 then
-
         --根据服务ID将消息分发给对应的服务处理
-        local destservice = msg[1]
-
-
-        if destservice ~= servicemsg.login then
-            --                
+        local dest, msghead, msgsub = string.byte(msg, 1, 3)
+        local msgid = msghead * 256 + msgsub
+        if service[msgid] then
+            skynet.send(service[msgid], "client", msgid, string.sub(msg, 4, -1), sz - 3)  
+        else
+            skynet.error("unkown msgid["..msgid.."]")
         end
-
-
     else
         print("error msg, disconnect client")
         skynet.send(agent[fd], "lua", "logout")
         gateserver.closeclient(fd)
         agent[fd] = nil
     end
-end
-
-function bytes_to_int(str,endian,signed) -- use length of string to determine 8,16,32,64 bits
-    local t={str:byte(1,-1)}
-    if endian=="big" then --reverse bytes
-        local tt={}
-        for k=1,#t do
-            tt[#t-k+1]=t[k]
-        end
-        t=tt
-    end
-    local n=0
-    for k=1,#t do
-        n=n+t[k]*2^((k-1)*8)
-    end
-    if signed then
-        n = (n > 2^(#t-1) -1) and (n - 2^#t) or n -- if last bit set, negative.
-    end
-    return n
 end
 
 gateserver.start(handler)
